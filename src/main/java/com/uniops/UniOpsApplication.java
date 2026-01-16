56556deccabf2154fd92249c335eeeb03bf5409e
ly <564066938@qq.com>
2026-01-16 10:13:26 +0800

    初始化

---
+++ src/main/java/com/uniops/UniOpsApplication.java
@@ -0,0 +1,13 @@
+package com.uniops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniOpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniOpsApplication.class, args);
+    }
+
+}
