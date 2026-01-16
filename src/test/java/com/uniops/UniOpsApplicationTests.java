965303404330c38d8039d42f721c6e0948371df1
ly <564066938@qq.com>
2026-01-16 10:18:58 +0800

    初始化

---
+++ src/test/java/com/uniops/UniOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniOpsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
